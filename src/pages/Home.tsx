43a7ed1a27161c7a71964d74eba37b11a0821b8e
baojiangli <2861166064@qq.com>
2024-01-16 11:05:52 +0800

    init

---
+++ src/pages/Home.tsx
@@ -0,0 +1,26 @@
+import './home.scss'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className='header'>
+        <div className='logo'>
+          logo
+        </div>
+        <div className='inner'>
+          <div className='left'>
+            left
+          </div>
+          <div className='right'>
+            right
+          </div>
+        </div>
+      </div>
+      <div className='title-area'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home;
